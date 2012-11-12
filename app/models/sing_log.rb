8b11d568e53bac0ecb59035d40f2cce18e846234
Yusaku ONO <yono.05@gmail.com>
2012-11-12 18:57:03 +0900

    fix sing_log index

--- app/models/sing_log.rb
+++ app/models/sing_log.rb
@@ -1,7 +1,11 @@
 class SingLog < ActiveRecord::Base
-  attr_accessible :song_id
+  attr_accessible :song_id, :sang_at
 
   belongs_to :song
 
   default_scope order('created_at')
+
+  def sang_at
+    self.created_at.strftime("%Y-%m-%d %H:%M")
+  end
 end
