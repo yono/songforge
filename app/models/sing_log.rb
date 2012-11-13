783dfaef93eb93b177f978eb6fba2497d077e7ab
Yusaku ONO <yono.05@gmail.com>
2012-11-13 09:48:59 +0900

    fixed #6

--- app/models/sing_log.rb
+++ app/models/sing_log.rb
@@ -3,7 +3,7 @@ class SingLog < ActiveRecord::Base
 
   belongs_to :song
 
-  default_scope order('created_at')
+  default_scope order('created_at DESC')
 
   def sang_at
     self.created_at.strftime("%Y-%m-%d %H:%M")
