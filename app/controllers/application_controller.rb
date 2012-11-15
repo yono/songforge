9acefc5399d4cf44d920d37dd6ad4a09d578da57
Yusaku ONO <yono.05@gmail.com>
2012-11-15 22:13:16 +0900

    fixed #18 basic auth when production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :auth
+
+  private
+  def auth
+    if Rails.env == 'production'
+      authenticate_or_request_with_http_basic do |user, pass|
+        user == ENV['BASIC_AUTH_USERNAME'] and pass == ENV['BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
 end
