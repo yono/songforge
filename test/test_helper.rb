bd99690a35ab19164b9975cc5b30469cd232f9a6
Yusaku ONO <yono.05@gmail.com>
2013-06-09 12:58:21 +0900

    fix test_helper to get coverage controller collectly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-require 'simplecov'
+
+require 'simplecov' # should require before #require 'rails/test_help'
 SimpleCov.start "rails"
+
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
