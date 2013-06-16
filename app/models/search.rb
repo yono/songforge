3c8e9424258323299166ef5bdfff34f4bd51b728
Yusaku ONO <yono.05@gmail.com>
2013-06-16 18:21:19 +0900

    fix bug about search

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,19 +5,16 @@ class Search
     already = Song.includes(:artist).where.not(:last_sang_at => nil)
 
     unless query.blank?
-      unsing = unsing.where(search_with_cond(query))
-      already = already.where(search_with_cond(query))
+      unsing = unsing.where(search_with_word(query))
+      already = already.where(search_with_word(query))
     end
 
     all_songs = unsing.to_a + already.to_a
   end
 
-  private
-
-    def search_with_word(query)
-      q = "%#{query}%"
-      search_cond = [Song, Artist].each do |obj|
-        obj.arel_table[:name].matches(q)
-      end.inject {|cond, i| cond.or i }
-    end
+  def self.search_with_word(query)
+    q = "%#{query}%"
+    [Song, Artist].map {|cond| cond.arel_table[:name].matches(q) }
+                  .inject {|cond, i| cond.or i }
+  end
 end
