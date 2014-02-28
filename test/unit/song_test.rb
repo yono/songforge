2631985aaf3c35a35dfcee9cb4ec587ee450651d
Yusaku ONO <yono.05@gmail.com>
2014-02-28 21:07:57 +0900

    add test

--- test/unit/song_test.rb
+++ test/unit/song_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class SongTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "unique name scope artist_id" do
     artist = Artist.create! :name => 'Hikaru Utada'
     song1 = Song.create! :name => 'Automatic', :artist_id => artist.id
     song2 = Song.new :name => 'Automatic', :artist_id => artist.id
     assert song2.invalid?
+  end
+
+  test "singing!" do
+    artist = Artist.create! :name => 'Hikaru Utada'
+    song1 = Song.create! :name => 'Automatic', :artist_id => artist.id
+    count = SingLog.count
+    song1.singing! 
+    assert_equal SingLog.count, (count + 1)
   end
 end
