85e527b102b7bb6c95608c521f0af3285902e5fc
Yusaku ONO <yono.05@gmail.com>
2014-03-02 11:47:55 +0900

    add song model spec

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe Song do
-  describe "validation" do
-    it "Song has name each artist" do
+  describe 'validation' do
+    it 'Song has name each artist' do
       artist = Artist.create! :name => 'Hikaru Utada'
       song1 = Song.create! :name => 'Automatic', :artist_id => artist.id
       song2 = Song.new :name => 'Automatic', :artist_id => artist.id
@@ -10,13 +10,25 @@ describe Song do
     end
   end
 
-  describe ".singing!" do
-    it "count up SingLog" do
+  describe '.singing!' do
+    it 'count up SingLog' do
       artist = Artist.create! :name => 'Hikaru Utada'
       song1 = Song.create! :name => 'Automatic', :artist_id => artist.id
       count = SingLog.count
       song1.singing! 
       SingLog.count.should == count + 1
+    end
+  end
+
+  describe '.artist_name=' do
+    context 'when args is present' do
+      it 'create artist' do
+        song = Song.create! :name => 'whiteout', :artist_name => 'Garnet Crow' 
+        Artist.where(name: 'Garnet Crow').present?.should be_true
+      end
+    end
+    context 'args is blank' do
+      pending 
     end
   end
 end
