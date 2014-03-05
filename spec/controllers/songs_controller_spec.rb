7570036bce071453b72e9e83586943b49a617594
Yusaku ONO <yono.05@gmail.com>
2014-03-05 22:16:12 +0900

    add songs controller spec

---
+++ spec/controllers/songs_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe SongsController do
+
+  describe 'GET index' do
+    pending
+  end
+
+  describe 'GET show' do
+    pending
+  end
+
+  describe 'GET new' do
+    pending
+  end
+
+  describe 'GET edit' do
+    pending
+  end
+
+  describe 'POST create' do
+    pending
+  end
+
+  describe 'PUT update' do
+    pending
+  end
+
+  describe 'DELETE destroy' do
+    pending
+  end
+
+  describe 'GET singing' do
+    pending
+  end
+
+  describe 'GET lyrics_image' do
+    pending
+  end
+
+end
