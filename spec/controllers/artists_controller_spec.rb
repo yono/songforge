0a83ab9d7d5b82847915b9e74e4f342544f8c10a
Yusaku ONO <yono.05@gmail.com>
2014-03-06 19:35:56 +0900

    add controller specs

---
+++ spec/controllers/artists_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe ArtistsController do
+
+  describe 'GET index' do
+    pending
+  end
+
+  describe 'GET show' do
+    pending
+  end
+
+  describe 'GET new' do
+    pending
+  end
+
+  describe 'GET edit' do
+    pending
+  end
+
+  describe 'POST create' do
+    pending
+  end
+
+  describe 'PUT update' do
+    pending
+  end
+
+  describe 'DELETE destroy' do
+    pending
+  end
+
+end
---
+++ spec/controllers/sing_logs_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe SingLogsController do
+  describe 'GET index' do
+    pending
+  end
+end
