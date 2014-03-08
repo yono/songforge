ddc27df7b67cf8cacb2bdfa3db98b721ea47ec06
Yusaku ONO <yono.05@gmail.com>
2014-03-08 22:25:03 +0900

    add sing_logs controller spec

--- spec/controllers/sing_logs_controller_spec.rb
+++ spec/controllers/sing_logs_controller_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe SingLogsController do
   describe 'GET index' do
-    pending
+    it "assigns @sing_logs" do
+      sing_log = SingLog.create!
+      get :index
+      expect(assigns(:sing_logs)).to eq([sing_log])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 end
