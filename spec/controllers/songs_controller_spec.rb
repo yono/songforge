13237bdb4de96a82ff0c26660f3901e50cd7ef70
Yusaku ONO <yono.05@gmail.com>
2014-03-10 20:16:34 +0900

    add specs

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -3,7 +3,16 @@ require 'spec_helper'
 describe SongsController do
 
   describe 'GET index' do
-    pending
+    it 'assigns @songs' do
+      song = Song.create! :name => 'Automatic'
+      get :index
+      expect(assigns(:songs)).to eq([song])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
   end
 
   describe 'GET show' do
