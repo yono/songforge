edaa5aaf4697ce1c10bc6c691fe287c86f5d5070
Yusaku ONO <yono.05@gmail.com>
2014-03-16 18:23:42 +0900

    rails update to 4.0.4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
 SongForge::Application.config.session_store :cookie_store, key: '_song_forge_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# SongForge::Application.config.session_store :active_record_store
