d097fb60017be70795d7918f6730c622b03c44c5
Yusaku ONO <yono.05@gmail.com>
2014-03-17 15:53:31 +0900

    add search spec

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Search do
+  before :each do
+  	Song.delete_all
+  end
+
+  context 'without query' do
+  	it 'returns all of songs' do
+      song1 = create(:song)
+      song2 = create(:song)
+      expect(Search.search('')).to match_array([song1, song2])
+  	end
+  end
+
+  context 'with query' do
+  	context 'matching query' do
+  	  it 'returns matched songs' do
+  	    song1 = create(:song, name: 'SHAKE')
+  	    song2 = create(:song, name: 'Thunderbird')
+  	    expect(Search.search('Thunderbird')).to include(song2)
+  	  end
+  	end
+
+  	context 'unmatching query' do
+  	  it 'does not return unmatched songs' do
+  	    song1 = create(:song, name: 'SHAKE')
+  	    song2 = create(:song, name: 'Thunderbird')
+  	    expect(Search.search('Thunderbird')).to_not include(song1)
+  	  end
+  	end
+  end
+end
