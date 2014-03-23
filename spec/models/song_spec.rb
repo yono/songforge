74cb8ff43f36ff55e7900ac3f5b3fd242b5bb366
Yusaku ONO <yono.05@gmail.com>
2014-03-23 19:21:06 +0900

    refactor song_spec

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,35 +2,33 @@ require "spec_helper"
 require 'tempfile'
 
 describe Song do
+  let(:song) { create(:song) }
+
   describe 'validation' do
-    it 'Song has name each artist' do
+    it "can't have same name wth a same artist" do
       artist = create(:artist)
-      song = create(:song, name: 'Automatic', artist_id: artist.id)
-      duplicate_song = build(:song, name: 'Automatic', artist_id: artist.id)
+      song.artist_id = artist.id
+      song.save
+      duplicate_song = build(:song, name: song.name, artist_id: artist.id)
+
       expect(duplicate_song.invalid?).to be true
     end
   end
 
-  describe '.singing!' do
-    it 'count up SingLog' do
-      song = build(:song)
-      expect{
-        song.singing! 
-      }.to change(SingLog, :count).by(1)
-    end
+  it '.singing!' do
+    expect{
+      song.singing! 
+    }.to change(SingLog, :count).by(1)
   end
 
   describe '.sang?' do
     context 'when not sang' do
-      it 'return false' do
-        song = build(:song)
-        expect(song.sang?).to be false
-      end
+      its(:sang?) { should be_false }
     end
 
     context 'when already sang' do
-      it 'return true' do
-        song = build(:song)
+      it 'returns true' do
+        song = create(:song)
         song.singing!
         expect(song.sang?).to be true
       end
@@ -39,92 +37,85 @@ describe Song do
 
   describe '.youtube_v' do
     context 'has no movie_url' do
-      it 'return nil' do
-        song = build(:song)
-        expect(song.youtube_v).to be nil
-      end
+      before { song.movie_url = nil }
+      its(:youtube_v) { should be_nil }
     end
 
     context 'has movie_url' do
-      it 'return youtube_v parameter' do
-        song = build(:song, movie_url: 'http://www.youtube.com/watch?v=abcdefghi')
-        expect(song.youtube_v).to eq('abcdefghi')
+      it 'returns value' do
+        song = create(:song)
+        song.movie_url = 'http://www.youtube.com/watch?v=abcdefghi'
+        expect(song.youtube_v).to eq "abcdefghi"
       end
     end
   end
 
   describe '.has_movie?' do
     context 'when has no movie_uel' do
-      it 'return false' do
-        song = build(:song)
-        expect(song.has_movie?).to be false
-      end
+      before { song.movie_url = nil }
+      its(:have_movie?) { false }
     end
 
     context 'when has movie_url' do
-      it 'return true' do
-        song = build(:song, movie_url: 'http://www.youtube.com/watch?v=abcdefghi')
-        expect(song.has_movie?).to be true
-      end
+      before { song.movie_url = 'http://www.youtube.com/watch?v=abcdefghi' }
+      its(:have_movie?) { true }
     end
   end
 
   describe '.lyrics_file=' do
-    it 'store content_type' do
-      song = build(:song)
+    before do
       tempfile = Tempfile::new("test.jpg", "#{Rails.root}/spec/images")
       tempfile.stub(:content_type).and_return('image/jpeg')
-
+      tempfile.stub(:read).and_return('binary')
       song.lyrics_file = tempfile
-      expect(song.content_type).to eq('image/jpeg')
     end
+
+    its(:content_type) { "image/jpeg" }
   end
 
   describe '.has_lyrics_file?' do
     context 'when has no lyrics file' do
-      it 'return false' do
-        song = build(:song)
-        expect(song.has_lyrics_file?).to be false
-      end
+      its(:has_lyrics_file?) { false }
     end
 
     context 'when has lyrics file' do
-      it 'return true' do
-        song = build(:song)
+      before do
         tempfile = Tempfile::new("test.jpg", "#{Rails.root}/spec/images")
         tempfile.stub(:content_type).and_return('image/jpeg')
         tempfile.stub(:read).and_return('binary')
-
         song.lyrics_file = tempfile
-        expect(song.has_lyrics_file?).to be true
       end
+
+      its(:has_lyrics_file?) { true }
     end
   end
 
   describe '.artist_name' do
-    it 'has no inplementation' do
-      song = create(:song)
-      expect(song.artist_name).to be nil
-    end
+    its(:artist_name) { nil }
   end
 
   describe '.artist_name=' do
     context 'when args is present' do
-      it 'create artist' do
-        song = build(:song, artist_name: 'Garnet Crow')
-        expect(Artist.where(name: 'Garnet Crow').present?).to be false
+      before { song.artist_name = 'Garnet Crow' }
 
-        # After save, persisted the artist.
-        song.save!
-        expect(Artist.where(name: 'Garnet Crow').present?).to be true
+      context 'before save' do
+        it "doesn't create artist" do
+          expect(Artist.where(name: 'Garnet Crow').present?).to be false
+        end
+
+        context 'after save' do
+          before { song.save }
+          it "create artist" do
+            expect(Artist.where(name: 'Garnet Crow').present?).to be true
+          end
+        end
       end
     end
 
     context 'args is blank' do
       it 'create no artist' do
-        artist_count = Artist.count
         expect {
-          song = build(:song)
+          new_song = build(:song)
         }.to change(Artist, :count).by(0)
       end
     end
