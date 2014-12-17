88d416feb443e215c7451e98dbdb2b6378fa5fc6
Yusaku ONO <yono.05@gmail.com>
2014-12-17 22:21:01 +0900

    fix index pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
   def new_button(button_label, path, options={})
-    options.merge!({type: 'button', class: 'btn', method: 'get', value: button_label, onclick: "location.href='#{path.to_s}'".html_safe})
+    options.merge!({type: 'button', class: 'btn btn-default', method: 'get', value: button_label, onclick: "location.href='#{path.to_s}'".html_safe})
     tag 'input', options
   end
 
   def edit_icon_link(path)
-    link_to '<i class="icon-pencil"></i>'.html_safe, path
+    link_to '<i class="fa fa-pencil"></i>'.html_safe, path
   end
 
   def delete_icon_link(delete_obj)
-    link_to '<i class="icon-trash"></i>'.html_safe, delete_obj, method: :delete, data: { confirm: 'Are you sure?' }
+    link_to '<i class="fa fa-trash-o"></i>'.html_safe, delete_obj, method: :delete, data: { confirm: 'Are you sure?' }
   end
 
   def table_css_classes
