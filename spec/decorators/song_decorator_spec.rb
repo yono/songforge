73f1f0ad75e37488b0074bcd62af16942a3ffa98
Yusaku ONO <yono.05@gmail.com>
2014-12-18 07:41:51 +0900

    fix spec

--- spec/decorators/song_decorator_spec.rb
+++ spec/decorators/song_decorator_spec.rb
@@ -7,7 +7,7 @@ describe SongDecorator do
         song = create(:song)
         song.singing!
         decorate song
-        expect(song.star_link).to eq("<a href=\"/songs/#{song.id}/singing\"><i class=\"icon-star\"></i></a>")
+        expect(song.star_link).to eq("<a href=\"/songs/#{song.id}/singing\"><i class=\"fa fa-star\"></i></a>")
       end
     end
 
@@ -15,7 +15,7 @@ describe SongDecorator do
       it 'returns link to blank star' do
         song = create(:song)
         decorate song
-        expect(song.star_link).to eq("<a href=\"/songs/#{song.id}/singing\"><i class=\"icon-star-empty\"></i></a>")
+        expect(song.star_link).to eq("<a href=\"/songs/#{song.id}/singing\"><i class=\"fa fa-star-o\"></i></a>")
       end
     end
   end
