df072391a098fd4ab33bbfae2e01310eeb3f097f
Yusaku ONO <yono.05@gmail.com>
2017-11-21 23:31:28 +0900

    update settings for rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,13 @@ Bundler.require(*Rails.groups)
 
 module SongForge
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
     config.i18n.default_locale = :ja
   end
 end
