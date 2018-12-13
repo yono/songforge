59b0fd955431a372d3be2e2d791e4f3b80e77f93
Yusaku ONO <yono.05@gmail.com>
2018-12-13 21:54:53 +0900

    Split mobile UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_request_variant
   protect_from_forgery
   include SessionsHelper
+
+  private
+
+  def set_request_variant
+    request.variant = :smartphone if request.from_smartphone?
+  end
 end
