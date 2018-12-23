d4cc67b677c6ddfd4ad71bbed365545785884fb5
Yusaku ONO <yono.05@gmail.com>
2018-12-23 14:56:03 +0900

    remove vue-swipe-actions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import store from '../store.js'
 import App from '../App.vue'
-import 'vue-swipe-actions/dist/vue-swipe-actions.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('hello'))
