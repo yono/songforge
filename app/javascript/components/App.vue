bd00627484e6b9ffe66dfce1368a90813698068c
Yusaku ONO <yono.05@gmail.com>
2019-01-04 02:10:55 +0900

    add footer

--- app/javascript/components/App.vue
+++ app/javascript/components/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <div id="header-fixed">
+    <div id="header">
       <h2>SongForge</h2>
     </div>
     <div id="body">
       <SongList/>
+    </div>
+    <div id="footer">
+      <span>footer</span>
     </div>
   </div>
 </template>
@@ -18,7 +21,7 @@ export default {
 </script>
 
 <style>
-div#header-fixed {
+div#header {
   position: sticky;
   top: 0px;
   bottom: 0px;
@@ -28,8 +31,15 @@ div#header-fixed {
   z-index: 1;
 }
 
-div#header-fixed > h2 {
+div#header> h2 {
   margin-bottom: 0;
+}
+
+div#footer {
+  position: sticky;
+  z-index: 1;
+  bottom: 0;
+  background-color: #999;
 }
 
 div#body {
