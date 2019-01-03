bcba87b6ba9beb9bf2d57e0e381f8885e2599024
Yusaku ONO <yono.05@gmail.com>
2019-01-03 18:14:34 +0900

    Fix header

--- app/javascript/components/App.vue
+++ app/javascript/components/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <h1>Songs</h1>
-    <SongList/>
+    <div id="header-fixed">
+      <h2>SongForge</h2>
+    </div>
+    <div id="body">
+      <SongList/>
+    </div>
   </div>
 </template>
 
@@ -12,3 +16,22 @@ export default {
   components: { SongList }
 }
 </script>
+
+<style>
+div#header-fixed {
+  position: sticky;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  background-color: #999;
+  z-index: 1;
+}
+
+div#header-fixed > h2 {
+  margin-bottom: 0;
+}
+
+div#body {
+}
+</style>
