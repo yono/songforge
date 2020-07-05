edeb4386052ef1bfff48328001a0ba420299a320
Yusaku ONO <yono.05@gmail.com>
2020-07-05 22:10:57 +0900

    Remove routes for mobile

--- app/controllers/mobile_controller.rb
+++
@@ -1,7 +0,0 @@
-class MobileController < ApplicationController
-  before_action :authenticate
-  skip_before_action :redirect_to_mobile
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/sp', to: 'mobile#index', as: :mobile
   resources :sessions, only: [:new, :create, :destroy]
   get    '/signin',  to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy'
@@ -18,8 +17,4 @@ Rails.application.routes.draw do
   resources :artists
 
   root 'songs#index'
-
-  namespace :api, defaults: { format: :json } do
-    resources :songs, only: :index
-  end
 end
