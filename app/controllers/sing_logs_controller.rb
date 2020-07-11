cee559b0b5003555142494aa28b053ff14e903c2
Yusaku ONO <yono.05@gmail.com>
2020-07-11 23:28:13 +0900

    Use preload

--- app/controllers/sing_logs_controller.rb
+++ app/controllers/sing_logs_controller.rb
@@ -2,7 +2,7 @@ class SingLogsController < ApplicationController
   before_action :authenticate
 
   def index
-    @sing_logs = SingLog.includes(song: [:artist]).page(params[:page]).per(20)
+    @sing_logs = SingLog.preload(song: [:artist]).page(params[:page]).per(20)
   end
 
   def destroy
