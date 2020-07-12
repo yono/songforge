2a691ebbc9a94d903117f802f84de6933e178800
Yusaku ONO <yono.05@gmail.com>
2020-07-12 21:12:51 +0900

    remove unused controller

--- app/controllers/api/songs_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::SongsController < ApplicationController
-  before_action :authenticate
-
-  def index
-    song_num = 100
-    @all_songs = Search.search(params[:q])
-    @songs = Kaminari.paginate_array(@all_songs).page(params[:page]).per(song_num)
-  end
-end
