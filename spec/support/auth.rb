e8ce9386ca2f068fec56a3f7ef814213f0304c18
Yusaku ONO <yono.05@gmail.com>
2020-07-14 02:51:16 +0900

    fix helper method

--- spec/support/auth.rb
+++ spec/support/auth.rb
@@ -1,3 +1,11 @@
 def test_sign_in
-  session[:username] = 'test'
+  allow(User).to receive(:username).and_return('user')
+  allow(User).to receive(:password).and_return('pass')
+
+  visit root_path
+
+  fill_in 'ユーザー名', with: 'user'
+  fill_in 'パスワード', with: 'pass'
+
+  click_on 'Sign in'
 end
