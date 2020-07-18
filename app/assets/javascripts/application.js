acf51ea2a82da0d2ca761ba9402b52d9369f91b6
Yusaku ONO <yono.05@gmail.com>
2020-07-18 17:59:31 +0900

    require rails-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
+//= require rails-ujs
 //= require jquery3
 //= require popper
 //= require bootstrap
