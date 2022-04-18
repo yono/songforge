37096c3f762d799514b2a1896f8b8eb19179889b
Yusaku ONO <yono.05@gmail.com>
2022-04-19 01:51:19 +0900

    remove sessions_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include SessionsHelper
+
+  helper_method :signed_in?
+
+  private
+
+  def sign_in(user)
+    session[:username] = user
+  end
+
+  def sign_out
+    reset_session
+  end
+
+  def authenticate
+    redirect_to signin_path unless signed_in?
+  end
+
+  def signed_in?
+    session[:username]
+  end
 end
